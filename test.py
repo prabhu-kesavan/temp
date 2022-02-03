689c9b68ce7fdb385581be035b9245fc37c8369c
prabhu-kesavan <kes.prabhu@gmail.com>
2022-02-03 18:54:56 +0530

    second version

--- test.py
+++ test.py
@@ -5,4 +5,10 @@ Created on Thu Feb  3 16:16:20 2022
 @author: prabh
 """
 
-print('This is for testing !!!')
+deci = input("Is this your first exercise !!")
+
+if deci == 'yes':
+    print('Good you have created a new file.  !!!')
+    
+else:
+    print('Try to create one !!!!. ')
